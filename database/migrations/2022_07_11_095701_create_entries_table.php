4fbbaf42552d624e7fd65a1186776d877d683fd0
OlhaRohoza <olha.rohoza@gmail.com>
2022-07-11 12:03:38 +0200

    laravel project

---
+++ database/migrations/2022_07_11_095701_create_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('date');
+            $table->integer('rate')->nullable();
+            $table->text('note');
+            // $table->foreignId('image_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+};
